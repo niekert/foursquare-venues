e34da2b99c46044e78a55fe727514c93ae50b0f5
Niek Kruse <niekkruse70@gmail.com>
2017-11-02 21:05:02 +0100

    Initial create-react-app commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer as ReactHotLoaderContainer } from 'react-hot-loader';
+import './index.css';
+import App from './scenes/AppContainer';
+import registerServiceWorker from './registerServiceWorker';
+
+const render = AppComponent => {
+  ReactDOM.render(
+    <ReactHotLoaderContainer>
+      <AppComponent />
+    </ReactHotLoaderContainer>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./scenes/AppContainer', () => {
+    render(App);
+  });
+}
+
+registerServiceWorker();
